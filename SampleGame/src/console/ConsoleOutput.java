e2d45a3164af406ff428017ebb243870f2ba7b0e
NaokiMatsuzawa <naokim1004@gmail.com>
2022-11-22 18:24:40 +0900

    各グリッドの状態取得を文字列ではなく列挙体を用いるようにした

--- SampleGame/src/console/ConsoleOutput.java
+++ SampleGame/src/console/ConsoleOutput.java
@@ -1,10 +1,31 @@
 package console;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import game_logic.MineSweeperGrid.GridType;;
+
 class ConsoleOutput {
-	public void DispMineSweeperField(game_logic.MineSweeperGrid[][] grids) {
+	private final Map<GridType, String> map = new HashMap<GridType,String>();
+
+	public ConsoleOutput() {
+		map.put(GridType.ZERO, "0");
+		map.put(GridType.ONE, "1");
+		map.put(GridType.TWO, "2");
+		map.put(GridType.THREE, "3");
+		map.put(GridType.FOUR, "4");
+		map.put(GridType.FIVE, "5");
+		map.put(GridType.SIX, "6");
+		map.put(GridType.SEVEN, "7");
+		map.put(GridType.EIGHT, "8");
+		map.put(GridType.BOMB, "B");
+		map.put(GridType.UNOPEN, "*");
+	}
+	
+	public void DispMineSweeperField(game_logic.MineSweeperGrid[][] grids) {		
 		for(game_logic.MineSweeperGrid[] cols: grids) {
 			for(game_logic.MineSweeperGrid grid : cols) {
-				System.out.print(grid.getString());
+				System.out.print(map.get(grid.getGridType()));
 			}
 			System.out.println();
 		}
--- SampleGame/src/game_logic/MineSweeperGrid.java
+++ SampleGame/src/game_logic/MineSweeperGrid.java
@@ -1,8 +1,25 @@
 package game_logic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public interface MineSweeperGrid {
+	enum GridType{
+		ZERO,
+		ONE,
+		TWO,
+		THREE,
+		FOUR,
+		FIVE,
+		SIX,
+		SEVEN,
+		EIGHT,
+		BOMB,
+		UNOPEN,
+	}
+	
 	void open();
-	String getString();
+	GridType getGridType();
 }
 
 abstract class MineSweeperGridBase implements MineSweeperGrid{
@@ -13,85 +30,81 @@ abstract class MineSweeperGridBase implements MineSweeperGrid{
 	protected GridState grid_state = GridState.UNOPEN;
 	
 	@Override
-	public String getString() {
-		switch(grid_state) {
-		case UNOPEN:
-			return "_";
-		case OPENED:
-			return getGridString();
-		default:
-			assert(false);
-		}
-		return " ";
+	public void open() {
+		grid_state = GridState.OPENED;
 	}
+	@Override
+	public GridType getGridType() {
+		if(isOpen()) return getGridTypeSub();
+		return GridType.UNOPEN;
+	}	
+
+
+	protected abstract GridType getGridTypeSub();
 	
 	boolean isOpen() {
 		return grid_state == GridState.OPENED;
 	}
 	
 	abstract boolean isZeroArround();
-	
-	abstract String getGridString();
 	abstract public boolean hasBomb();
 }
 
 class MineSweeperBombGrid extends MineSweeperGridBase{
 
 	@Override
-	public void open() {
-		// TODO ゲームオーバー処理に入る？
-		grid_state = GridState.OPENED;
-		assert(false);
+	protected GridType getGridTypeSub() {
+		return GridType.BOMB;
 	}
-
-
-
+	
 	@Override
 	public boolean hasBomb() {
 		return true;
 	}
 
-
-
-	@Override
-	String getGridString() {
-		return "B";
-	}
-
-
-
 	@Override
 	boolean isZeroArround() {
 		return false;
-	}	
+	}
+
+
+
 }
 
 class MineSweeperNormalGrid extends MineSweeperGridBase{
-	private final int number_of_mines_arround;
+	private final int number_of_mines_around;
+	private static final Map<Integer, GridType> map = new HashMap<Integer, GridType>();
 	
-	public MineSweeperNormalGrid(int number_of_mines_arround) {
-		this.number_of_mines_arround = number_of_mines_arround;
+	public MineSweeperNormalGrid(int number_of_mines_around) {
+		assert(number_of_mines_around >= 0 && number_of_mines_around <= 8);
+		this.number_of_mines_around = number_of_mines_around;
+		map.put(0, GridType.ZERO);
+		map.put(1, GridType.ONE);
+		map.put(2, GridType.TWO);
+		map.put(3, GridType.THREE);
+		map.put(4, GridType.FOUR);
+		map.put(5, GridType.FIVE);
+		map.put(6, GridType.SIX);
+		map.put(7, GridType.SEVEN);
+		map.put(8, GridType.EIGHT);
 	}
 	
 	@Override
-	public void open() {
-		grid_state = GridState.OPENED;
+	protected GridType getGridTypeSub() {
+		return map.get(number_of_mines_around);
 	}
 
+	
 	@Override
 	public boolean hasBomb() {
 		return false;
 	}
 
 	@Override
-	String getGridString() {
-		return String.valueOf(number_of_mines_arround);
+	boolean isZeroArround() {
+		return number_of_mines_around == 0;
 	}
 
-	@Override
-	boolean isZeroArround() {
-		return number_of_mines_arround == 0;
-	}
-	
+
 }
 
